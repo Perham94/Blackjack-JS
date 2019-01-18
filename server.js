c8188953604cf386b9b0f7d826c1f9afe4af70b7
Chjun-chi Chiu <44897681+Xarczuz@users.noreply.github.com>
2019-01-18 20:18:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const express = require('express');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3000;
+const port = process.env.PORT;
 const blackjack = require('./blackjack');
 
 process.on('uncaughtException', function (err) {
